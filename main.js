b6ec311696f23b0097c7f7c8887da11254242f78
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-16 01:59:02 -0500

    Save button eventListener added.

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ console.log('javascript connected.')
 
 // GLOBAL VARIABLES ===============================
 const url = "http://localhost:3000/notes"
-const form = document.getElementById()
+const form = document.getElementById('notes-form')
+
+// EL FUNCTIONS
+function noteSubmit(event) {
+  event.preventDefault()
+  console.log('Save button was pressed!')
+}
 
 // EVENT LISTENERS ================================
+form.addEventListener('submit', noteSubmit)
+
 
 // CRUD FUNCTIONS =================================
 // Create
