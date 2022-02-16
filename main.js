50168fc6feca42b076999a9d4844c3c897f980f3
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-16 01:44:37 -0500

    Sketched out 'skeleton' sections in JavaScript

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 console.log('javascript connected.')
 
+// GLOBAL VARIABLES ===============================
+const url = "http://localhost:3000/notes"
+const form = document.getElementById()
+
+// EVENT LISTENERS ================================
+
+// CRUD FUNCTIONS =================================
+// Create
+// Read
+// Update
+// Delete
+
+// DOM FUNCTIONS ==================================
 
 
-
-
-
-
+// CALL MAIN FUNCTION =============================
